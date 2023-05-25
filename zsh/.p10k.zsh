96f74547898078db6a40d5081e431f56bbb9a7b8
Marcel <firebird10@gmx.de>
2023-05-25 19:31:19 +0200

    zsh: change icon

--- zsh/.p10k.zsh
+++ zsh/.p10k.zsh
@@ -175,7 +175,7 @@
   # OS identifier color.
   typeset -g POWERLEVEL9K_OS_ICON_FOREGROUND=
   # Make the icon bold.
-  typeset -g POWERLEVEL9K_OS_ICON_CONTENT_EXPANSION='🦉'
+  typeset -g POWERLEVEL9K_OS_ICON_CONTENT_EXPANSION='⛩️'
 
   ################################[ prompt_char: prompt symbol ]################################
   # Green prompt symbol if the last command succeeded.
